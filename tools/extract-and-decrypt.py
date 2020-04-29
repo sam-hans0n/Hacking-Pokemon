5470b1b29ab56e45acb2dd25b3e73e7c8561f77d
sam-hans0n <hans5376@umn.edu>
2020-04-28 22:55:30 -0500

    the end is in sight

--- tools/extract-and-decrypt.py
+++ tools/extract-and-decrypt.py
@@ -1,4 +1,3 @@
-
 # ------------------------------------------------------------------------------
 # Sam Hanson, 2020
 #
@@ -16,20 +15,22 @@
 # ------------------------------------------------------------------------------
 
 
+
+
+
 party_data_raw = "C1E007DD54E18D05BDC2BBCCC7BBC8BEBFCC0202BBBBBBBBBBBBBB00DA1200009F01A7D895018AD8B6298AD895018AD895018AD895018AD895598FFA75FF9AF395018AD891018AD812018AD895478AD80000000005FF120012000B000A000B000B000B0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000FF000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000FF0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
 
-
-def ExtractBytesFromIndex(byte_str, start, end):
-    bytes = byte_str[start*2:end*2] # 2 because there are 2 chars ber byte
-
-    # also reorders bytes from little endian to big endian format
+def ToBigEndian(byte_str):
     bytes_arr = []
-    for i in range(0, len(bytes), 2):
-        bytes_arr.append(bytes[i:i+2])
+    for i in range(0, len(byte_str), 2):
+        bytes_arr.append(byte_str[i:i+2])
     bytes_arr = list(reversed(bytes_arr))
 
     bytes_little_endian = ''.join(bytes_arr) # essentially, the .implode() function from PHP
     return bytes_little_endian
+
+def ExtractBytesFromIndex(byte_str, start, end):
+    return byte_str[start*2:end*2] # 2 because there are 2 chars ber byte
 
 
 class PokemonParty:
@@ -43,7 +44,7 @@ class PokemonParty:
         self.markings = ExtractBytesFromIndex(self.party_data_raw, 27, 28) # offset 27
         self.checksum = ExtractBytesFromIndex(self.party_data_raw, 28, 30) # offset 28
         self.unknown = ExtractBytesFromIndex(self.party_data_raw, 30, 32) # offset 30
-        self.data = PokemonData(ExtractBytesFromIndex(self.party_data_raw, 32, 80), self.personality_value) # offset 32
+        self.data = PokemonData(ExtractBytesFromIndex(self.party_data_raw, 32, 80), self.personality_value, self.OT_ID) # offset 32
         self.status = ExtractBytesFromIndex(self.party_data_raw, 80, 84) # offset 80
         self.level = ExtractBytesFromIndex(self.party_data_raw, 84, 85) # offset 84
         self.pokerus = ExtractBytesFromIndex(self.party_data_raw, 85, 86) # offset 85
@@ -58,23 +59,32 @@ class PokemonParty:
 
 
 class PokemonData:
-    def __init__(self, data, personality_value):
+    def __init__(self, data, personality_value, OT_ID):
         self.data = data
-
-        self.key = self.Decrypt_PokemonData(self.data) # to be decrypted...
+        self.personality_value = personality_value
+        self.OT_ID = OT_ID
 
         self.order = "" # substructures are randomly ordered
         self.growth = None
         self.attack = None
         self.ev = None
         self.misc = None
-        self.InitiateSubstructes()
+        self.PopulateSubstructureData()
+
+        self.key = ""
+        self.Decrypt_PokemonData() # decrypt substructure data
 
 
-    def Determine_Structure_Order(self, personality_value):
-        order_num = int(personality_value, 16) % 24 # formula for finding struct order
 
-        substructs = []
+    def PopulateSubstructureData(self):
+        ## This functions exists for a couple reasons:
+        #    a) Find the substructure order, since the game "randomly" generates
+        #       it.
+        #    b) Correctly populate substructure with extracted bytes from
+        #       PokemonData struct. Specifically: order, growth, attack, ev,
+        #       and misc properties.
+
+        order_num = int(self.personality_value, 16) % 24 # formula for finding struct order
         if order_num == 0:
             self.order = "GAEM"
             self.growth = Growth(ExtractBytesFromIndex(self.data, 0, 12))
@@ -134,7 +144,7 @@ class PokemonData:
             self.attack = Attacks(ExtractBytesFromIndex(self.data, 0, 12))
             self.ev = EV_Condition(ExtractBytesFromIndex(self.data, 12, 24))
             self.misc = Miscellaneous(ExtractBytesFromIndex(self.data, 24, 36))
-            self.growth = Growth(ExtractBytesFromIndex(self.data, 36, 48))
+            self.growth = Growth(ToBigEndian(ExtractBytesFromIndex(self.data, 36, 48)))
         elif order_num == 10:
             self.order = "AMGE"
             self.attack = Attacks(ExtractBytesFromIndex(self.data, 0, 12))
@@ -166,35 +176,80 @@ class PokemonData:
             self.growth = Growth(ExtractBytesFromIndex(self.data, 24, 36))
             self.misc = Miscellaneous(ExtractBytesFromIndex(self.data, 36, 48))
         elif order_num == 15:
-            return "EAMG"
+            self.order = "EAMG"
+            self.ev = EV_Condition(ExtractBytesFromIndex(self.data, 0, 12))
+            self.attack = Attacks(ExtractBytesFromIndex(self.data, 12, 24))
+            self.miscellaneous = Miscellaneous(ExtractBytesFromIndex(self.data, 24, 36))
+            self.Growth = Growth(ExtractBytesFromIndex(self.data, 36, 48))
         elif order_num == 16:
-            return "EMGA"
+            self.order = "EMGA"
+            self.ev = EV_Condition(ExtractBytesFromIndex(self.data, 0, 12))
+            self.miscellaneous = Miscellaneous(ExtractBytesFromIndex(self.data, 12, 24))
+            self.growth = Growth(ExtractBytesFromIndex(self.data, 24, 36))
+            self.attack = Attacks(ExtractBytesFromIndex(self.data, 36, 48))
         elif order_num == 17:
-            return "EMAG"
+            self.order = "EMAG"
+            self.ev = EV_Condition(ExtractBytesFromIndex(self.data, 0, 12))
+            self.miscellaneous = Miscellaneous(ExtractBytesFromIndex(self.data, 12, 24))
+            self.attack = Attacks(ExtractBytesFromIndex(self.data, 24, 36))
+            self.growth = Growth(ExtractBytesFromIndex(self.data, 36, 48))
         elif order_num == 18:
-            return "MGAE"
+            self.order = "MGAE"
+            self.miscellaneous = Miscellaneous(ExtractBytesFromIndex(self.data, 0, 12))
+            self.growth = Growth(ExtractBytesFromIndex(self.data, 12, 24))
+            self.attack = Attacks(ExtractBytesFromIndex(self.data, 24, 36))
+            self.ev = EV_Condition(ExtractBytesFromIndex(self.data, 36, 48))
         elif order_num == 19:
-            return "MGEA"
+            self.order = "MGEA"
+            self.miscellaneous = Miscellaneous(ExtractBytesFromIndex(self.data, 0, 12))
+            self.growth = Growth(ExtractBytesFromIndex(self.data, 12, 24))
+            self.ev = EV_Condition(ExtractBytesFromIndex(self.data, 24, 36))
+            self.attack = Attacks(ExtractBytesFromIndex(self.data, 36, 48))
         elif order_num == 20:
-            return "MAGE"
-        elif order == 21:
-            return "MAEG"
-        elif order == 22:
-            return "MEGA"
-        elif order == 23:
-            return "MEAG"
+            self.order = "MAGE"
+            self.miscellaneous = Miscellaneous(ExtractBytesFromIndex(self.data, 0, 12))
+            self.attack = Attacks(ExtractBytesFromIndex(self.data, 12, 24))
+            self.growth = Growth(ExtractBytesFromIndex(self.data, 24, 36))
+            self.ev = EV_Condition(ExtractBytesFromIndex(self.data, 36, 48))
+        elif order_num == 21:
+            self.order = "MAEG"
+            self.miscellaneous = Miscellaneous(ExtractBytesFromIndex(self.data, 0, 12))
+            self.attack = Attacks(ExtractBytesFromIndex(self.data, 12, 24))
+            self.ev = EV_Condition(ExtractBytesFromIndex(self.data, 24, 36))
+            self.growth = Growth(ExtractBytesFromIndex(self.data, 36, 48))
+        elif order_num == 22:
+            self.order = "MEGA"
+            self.miscellaneous = Miscellaneous(ExtractBytesFromIndex(self.data, 0, 12))
+            self.ev = EV_Condition(ExtractBytesFromIndex(self.data, 12, 24))
+            self.growth = Growth(ExtractBytesFromIndex(self.data, 24, 36))
+            self.attack = Attacks(ExtractBytesFromIndex(self.data, 36, 48))
+        elif order_num == 23:
+            self.order = "MEAG"
+            self.miscellaneous = Miscellaneous(ExtractBytesFromIndex(self.data, 0, 12))
+            self.ev = EV_Condition(ExtractBytesFromIndex(self.data, 12, 24))
+            self.attack = Attacks(ExtractBytesFromIndex(self.data, 24, 36))
+            self.growth = Growth(ExtractBytesFromIndex(self.data, 36, 48))
 
-        return substructs
 
 
-    def Decrypt_PokemonData(self, data):
-        return None
+    def Decrypt_PokemonData(self):
+
+        # Need 32-bit (4 byte) decryption key
+        print(self.personality_value)
+        print(self.OT_ID)
+        print("personality - dec: " + str(int(self.personality_value, 16)))
+        print("ot_id - dec: " + str(int(self.OT_ID, 16)))
+        print("ANS: " + str(int(self.OT_ID, 16) ^ int(self.personality_value, 16)))
+        self.key = int(self.personality_value, 16) ^ int(self.OT_ID, 16)
+        print("KEY = " + str(self.key))
+        print("self.growth.species: " + self.growth.species)
+        species = int(self.growth.species, 16) ^ self.key
 
 
 class Growth:
     def __init__(self, data):
         self.data = data
-        self.species = ExtractBytesFromIndex(data, 0, 2) # offset 0
+        self.species = ToBigEndian(ExtractBytesFromIndex(data, 0, 2)) # offset 0
         self.item_held = ExtractBytesFromIndex(data, 2, 4) # offset 2
         self.experience = ExtractBytesFromIndex(data, 4, 8) # offset 4
         self.pp_bonuses = ExtractBytesFromIndex(data, 8, 9) # offset 8
@@ -242,30 +297,70 @@ class Miscellaneous:
 
 
 party = PokemonParty(party_data_raw)
-print(party.party_data_raw + "\n")
+# print(party.party_data_raw + "\n")
+#
+# print(party.personality_value)
+# print(party.OT_ID)
+# print(party.nickname)
+# print(party.language)
+# print(party.OT_name)
+# print(party.markings)
+# print(party.checksum)
+# print(party.unknown)
+# print(party.status)
+# print(party.level)
+# print(party.pokerus)
+# print(party.curr_hp)
+# print(party.tot_hp)
+# print(party.attack)
+# print(party.defense)
+# print(party.speed)
+# print(party.sp_attack)
+# print(party.sp_defense)
+# print("\n\n")
 
-print(party.personality_value)
-print(party.OT_ID)
-print(party.nickname)
-print(party.language)
-print(party.OT_name)
-print(party.markings)
-print(party.checksum)
-print(party.unknown)
 
-pok_data = party.data
-print("\n  * " + pok_data.data)
 
-growth_sec = pok_data.growth
-print("\n  * " + growth_sec.data)
 
-print(party.status)
-print(party.level)
-print(party.pokerus)
-print(party.curr_hp)
-print(party.tot_hp)
-print(party.attack)
-print(party.defense)
-print(party.speed)
-print(party.sp_attack)
-print(party.sp_defense)
+# pok_data_order = party.data.order
+# print("pok_data_order: " + pok_data_order)
+# print("party.data.growth.data: " + party.data.growth.data)
+# print("party.data.growth.species: " + party.data.growth.species)
+# print("party.data.misc.data: " + party.data.misc.data)
+# print("\n")
+
+
+
+# personality_value = DD07E0C1 = 3708281025
+# order_num = personality_value % 24 = 9
+# order = AEMG
+# OT_ID = 058DE154 = 93184340
+# growth.species = D88A0191
+# key = OT_ID XOR personality_value = 058DE154 XOR DD07E0C1 = d88a0195
+# pokemon species = d88a0195 XOR D88A0191 = 4
+
+
+pokemon = party.data
+
+print("personality_value = " + pokemon.personality_value)
+print("personality - dec: " + str(int(pokemon.personality_value, 16)))
+print("\n")
+
+print("OT_ID = " + pokemon.OT_ID)
+print("\n")
+
+print("pokemon.growth.species = " + pokemon.growth.species)
+print("DATA: " + pokemon.growth.data)
+print("\n")
+
+print("KEY = " + str(pokemon.key))
+print("\n")
+
+print("ANS: " + str(int(pokemon.OT_ID, 16) ^ int(pokemon.personality_value, 16)))
+print("\n")
+
+
+
+
+
+# --
